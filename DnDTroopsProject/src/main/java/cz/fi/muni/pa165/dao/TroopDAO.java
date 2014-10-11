a10f1ff473e3998e370d0fae52f5a0985cdcb0bf
Andrej Nemec <nemec@ics.muni.cz>
2014-10-11 18:16:47 +0200

    DAO filled with CRUD

--- DnDTroopsProject/src/main/java/cz/fi/muni/pa165/dao/TroopDAO.java
+++ DnDTroopsProject/src/main/java/cz/fi/muni/pa165/dao/TroopDAO.java
@@ -5,10 +5,64 @@
  */
 package cz.fi.muni.pa165.dao;
 
+import cz.fi.muni.pa165.entity.Troop;
+import java.util.List;
+
 /**
  *
- * @author Andrej
+ * @author Andrej Nemec
+ * @UCO 396474
  */
-public class TroopDAO {
+public interface TroopDAO {
+ 
+    /**
+     * Add new troop into database. Id is automatically generated and
+     * stored into id attribute.
+     *
+     * @param troop troop to be created.
+     * @throws IllegalArgumentException when troop is null, troop already has id assigned
+     */
+    public void createTroop(Troop troop) throws IllegalArgumentException;
     
+    /**
+     * Return troop with given id.
+     *
+     * @param id primary key of troop.
+     * @return troop with given id or null if such troop is not in database.
+     * @throws IllegalArgumentException when given id is null.
+     */
+    public Troop getTroop(Long id) throws IllegalArgumentException;
+    
+    /**
+     * Updates troop in database.
+     *
+     * @param troop troop to be updated.
+     * @throws IllegalArgumentException when troop is null, or has null id.
+     */
+    void updateTroop(Troop troop) throws IllegalArgumentException;
+    
+    /**
+     * Remove troop from database.
+     *
+     * @param troop troop to be removed from database.
+     * @throws IllegalArgumentException when troop is null, or has null id.
+     */
+    void removeTroop(Troop troop) throws IllegalArgumentException;
+    
+    /**
+     * Return list of all troops in the database.
+     *
+     * @return list of all troops.
+     */
+    List<Troop> getAllTroops();
+    
+    /**
+     * Method to find troop by name.
+     * 
+     * @param name name to find troop
+     * @return list of all troops with given name.
+     * @throws IllegalArgumentException when given name is null.
+     */
+
+    List<Troop> findTroopByName(String Name) throws IllegalArgumentException;
 }
