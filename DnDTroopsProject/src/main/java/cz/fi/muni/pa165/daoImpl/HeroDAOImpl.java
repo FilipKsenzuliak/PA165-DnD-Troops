207a532ed4a84090eecb6841ab51258e28184e06
FilipKsenzuliak <filipksenzuliak@gmail.com>
2014-10-11 23:48:40 +0200

    Implementing HeroDAOImpl methods

--- DnDTroopsProject/src/main/java/cz/fi/muni/pa165/daoImpl/HeroDAOImpl.java
+++ DnDTroopsProject/src/main/java/cz/fi/muni/pa165/daoImpl/HeroDAOImpl.java
@@ -9,11 +9,13 @@ import cz.fi.muni.pa165.entity.Hero;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import org.springframework.stereotype.Repository;
 
 /**
  *
- * @author Filip
+ * @author Filip Ksenzuliak
+ * @uco 396072
  */
 @Repository
 public class HeroDAOImpl implements HeroDAO{
@@ -34,27 +36,58 @@ public class HeroDAOImpl implements HeroDAO{
 
     @Override
     public Hero getHero(Long id) throws IllegalArgumentException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if(id == null) {
+            throw new IllegalArgumentException("getHero called with null.");
+        }
+        
+        Hero result = entityManager.find(Hero.class,id);
+        entityManager.detach(result);
+        return result;
     }
 
     @Override
     public void updateHero(Hero hero) throws IllegalArgumentException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if(hero == null || hero.getId() != null || hero.getRace() == null || 
+                hero.getAge() == 0 || hero.getLevel() == 0 ||
+                hero.getRole() == null || hero.getTroop() == null) {
+            throw new IllegalArgumentException("Update hero called with wrong param.");
+        }
+        
+        Hero find = entityManager.find(Hero.class, hero.getId());
+        if(find == null) {
+            throw new IllegalArgumentException("Hero is not present in db.");
+        }
+        
+        entityManager.merge(hero);
     }
 
     @Override
     public void removeHero(Hero hero) throws IllegalArgumentException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if(hero == null || hero.getId() == null) {
+            throw new IllegalArgumentException("Remove hero called with wrong param");
+        }
+        Hero result = entityManager.find(Hero.class,hero.getId());
+        entityManager.remove(result);
     }
 
     @Override
     public List<Hero> getAllHeroes() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<Hero> heroes;
+        
+        Query query = entityManager.createQuery("SELECT c FROM Customer c ORDER BY c.id");
+        heroes = query.getResultList();
+        entityManager.detach(heroes);
+        return heroes;
     }
 
     @Override
     public List<Hero> findHeroByName(String Name) throws IllegalArgumentException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<Hero> hero;
+        Query query = entityManager.createQuery("SELECT c FROM Customer c WHERE c.name = :name ORDER BY c.id");
+        //query.setParameter("name", name);
+        hero = query.getResultList();
+        entityManager.detach(hero);
+        return hero;
     }
     
 }
