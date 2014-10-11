b112befbec5dda2f853fcef6a29e86947e7696af
hubacdavid <396042@mail.muni.cz>
2014-10-11 18:04:57 +0200

    sdf

--- DnDTroopsProject/src/main/java/cz/fi/muni/pa165/AppMain.java
+++ DnDTroopsProject/src/main/java/cz/fi/muni/pa165/AppMain.java
@@ -19,30 +19,6 @@ public class AppMain {
 	public static void main(String[] args) throws SQLException {
 		//The following line is here just to start up a in-memory database 
 		new AnnotationConfigApplicationContext(DaoContext.class);
-		
-		System.out.println(" ****** STARTING PET STORE APPLICATOIN ****** ");
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("myUnit");
-		EntityManager em = emf.createEntityManager();
-		em.getTransaction().begin();
-		
-		Calendar cal = Calendar.getInstance();
-		
-		printAllCages(emf);
-                emf.close();
-	}
-	
-	public static void printAllCages(EntityManagerFactory emf){
-		System.out.println(" ********************************");
-		System.out.println("        CAGES LIST      ");
-		EntityManager em = emf.createEntityManager();
-		List<Role> cages = em.createQuery("SELECT c from Cage c",Role.class).getResultList();
-		
-		for (Role c : cages) {
-			System.out.println(c);
-		}
-		
-		em.close();
-		
-	}
+        }
 
 }
