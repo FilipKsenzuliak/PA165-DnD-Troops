a721d6ff6cd1eb2dc239d99827bb850c35a2ae1b
Tomas Javorsky <tomuso.o@gmail.com>
2014-10-11 19:35:05 +0200

    docs

--- DnDTroopsProject/src/main/java/cz/fi/muni/pa165/dao/MissionDAO.java
+++ DnDTroopsProject/src/main/java/cz/fi/muni/pa165/dao/MissionDAO.java
@@ -11,14 +11,45 @@ import java.util.List;
 
 public interface MissionDAO {
     
+     /**
+     * Add new m into database. Id is automatically generated and
+     * stored into id attribute.
+     *
+     * @param mission mission to be created.
+     * @throws IllegalArgumentException when mission is null, mission already has id assigned
+     */
     public void createMission(Mission mission) throws IllegalArgumentException;
     
+    /**
+     * Return mission with given id.
+     *
+     * @param id primary key of mission.
+     * @return mission with given id or null if such hero is not in database.
+     * @throws IllegalArgumentException when given id is null.
+     */
     public Mission getMissionById(Long id) throws IllegalArgumentException;
     
+    /**
+     * Updates mission in database.
+     *
+     * @param mission mission to be updated.
+     * @throws IllegalArgumentException when mission is null, or has null id.
+     */
     public void updateMission(Mission mission) throws IllegalArgumentException;
     
+    /**
+     * Remove mission from database.
+     *
+     * @param mission mission to be removed from database.
+     * @throws IllegalArgumentException when mission is null, or has null id.
+     */
     public void deleteMission(Mission mission) throws IllegalArgumentException;
     
+    /**
+     * Return list of all missions in the database.
+     *
+     * @return list of all missions.
+     */
     public List<Mission> getAllMissions();
     
 }
