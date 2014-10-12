702f0c170aa140d002efdaa55d07ebffe6647285
FilipKsenzuliak <filipksenzuliak@gmail.com>
2014-10-12 19:42:47 +0200

    deleting log4j and creating application context

--- DnDTroopsProject/src/test/java/cz/fi/muni/pa165/test/MissionDAOTest.java
+++ DnDTroopsProject/src/test/java/cz/fi/muni/pa165/test/MissionDAOTest.java
@@ -4,76 +4,77 @@
  */
 package cz.fi.muni.pa165.test;
 
-import cz.fi.muni.pa165.DaoContext;
-import java.util.Arrays;
-import java.util.Calendar;
-import java.util.Date;
+import cz.fi.muni.pa165.dao.MissionDAO;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceUnit;
-import javax.persistence.spi.LoadState;
+import static org.junit.Assert.*;
 
-import org.hibernate.annotations.OnDeleteAction;
-import org.hibernate.jpa.internal.util.PersistenceUtilHelper;
 import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.annotation.DirtiesContext.ClassMode;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTransactionalTestNGSpringContextTests;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.testng.annotations.Test;
 
 import cz.fi.muni.pa165.entity.Mission;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
 
 /**
  *
  * @author Filip Ksenzuliak
  * @uco 396072
  */
-@ContextConfiguration(classes=DaoContext.class)
-@DirtiesContext(classMode=ClassMode.AFTER_EACH_TEST_METHOD)
-public class MissionDAOTest {
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "/application-context.xml"})
+@TransactionConfiguration(defaultRollback=true)
+public class MissionDAOTest extends AbstractTestNGSpringContextTests {
         
-        @PersistenceUnit
-	public EntityManagerFactory emf;
-	
-	@DirtiesContext
-	@BeforeMethod
-	public void setup(){
-		EntityManager em = emf.createEntityManager();
-		
-		em.getTransaction().begin();
-		
-		Mission mis1 = new Mission();
-		mis1.setName("Dragon above us");		
-		mis1.setObjective("Find that filthy dragon and slay him");
-		mis1.setReward(150);
-		
-		Mission mis2 = new Mission();
-		mis2.setName("Not a dragon above us");		
-		mis2.setObjective("Find something and slay it");
-		mis2.setReward(50);
-		
-		Mission mis3 = new Mission();
-		mis3.setName("Relaxing");		
-		mis3.setObjective("Take a break and relax");
-		mis3.setReward(0);
-		
-		em.persist(mis1);
-		em.persist(mis2);
-		em.persist(mis3);
-		em.getTransaction().commit();
-		em.close();
-	}
-        
-        @Test
-	public void findAllMissions() {
-		EntityManager em = emf.createEntityManager();
-		List<Mission> missions = em.createQuery("SELECT p FROM Mission p",Mission.class).getResultList();
-		em.close();
-		
-		Assert.assertEquals(missions.size(), 3);
-	}
+    @Autowired
+    MissionDAO missionDAO;
+    
+    public MissionDAOTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+    
+    @Test
+    public void testFindAllAccounts() {
+        List<Mission> miss = missionDAO.getAllMissions();
+        assertEquals("Check DB is empty first", 0, miss.size());
+        Mission m = new Mission();
+        m.setName("nieco");
+        m.setObjective("kill it with fire");
+        m.setReward(150);
+        missionDAO.createMission(m);
+        miss = missionDAO.getAllMissions();
+        assertEquals("check Account has been created", 1, miss.size());
+    }
 }
