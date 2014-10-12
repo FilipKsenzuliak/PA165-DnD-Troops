4370b15d3440bf656becfe8241fc491ea699eb82
Tomas Javorsky <tomuso.o@gmail.com>
2014-10-12 22:04:57 +0200

    Entity a Repository finished

--- gs-accessing-data-jpa-master/complete/src/main/java/source/HeroRepository.java
+++ gs-accessing-data-jpa-master/complete/src/main/java/source/HeroRepository.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package source;
 
-/**
- *
- * @author Eduard
- */
-public interface HeroRepository {
-    
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface HeroRepository extends CrudRepository<Hero, Long> {
+
+    List<Hero> findByName(String name);
 }
---
+++ gs-accessing-data-jpa-master/complete/src/main/java/source/RoleRepository.java
@@ -0,0 +1,10 @@
+package source;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface RoleRepository extends CrudRepository<Role, Long> {
+
+    List<Role> findByName(String name);
+}
