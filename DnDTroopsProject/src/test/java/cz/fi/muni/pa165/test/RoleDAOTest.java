185e50f7742afb329b9c2cd4da007186d3573297
FilipKsenzuliak <filipksenzuliak@gmail.com>
2014-10-12 12:37:45 +0200

    Creating role test class

---
+++ DnDTroopsProject/src/test/java/cz/fi/muni/pa165/test/RoleDAOTest.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.fi.muni.pa165.test;
+
+/**
+ *
+ * @author Filip Ksenzuliak
+ * @uco 396072
+ */
+public class RoleDAOTest {
+    
+}
