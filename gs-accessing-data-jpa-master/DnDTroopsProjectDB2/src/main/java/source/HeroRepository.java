969c2ddb8ef9e8d2de6ac8e3b5d9b8375e97d651
Tomas Javorsky <tomuso.o@gmail.com>
2014-10-12 22:26:06 +0200

    javadoc v repo

--- gs-accessing-data-jpa-master/DnDTroopsProjectDB2/src/main/java/source/HeroRepository.java
+++ gs-accessing-data-jpa-master/DnDTroopsProjectDB2/src/main/java/source/HeroRepository.java
@@ -6,5 +6,12 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface HeroRepository extends CrudRepository<Hero, Long> {
 
+    
+    /**
+     * Method to find by name.
+     * 
+     * @param name name to find hero
+     * @return list of all heroes with given name.
+     */
     List<Hero> findByName(String name);
 }
