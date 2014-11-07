9695dddd28e10e1c38108cc7458c65d0e2730302
7Tomus <tomuso.o@gmail.com>
2014-11-07 22:57:51 +0100

    troop

--- DnDTroopsProject/src/main/java/cz/fi/muni/pa165/daoImpl/TroopDAOImpl.java
+++ DnDTroopsProject/src/main/java/cz/fi/muni/pa165/daoImpl/TroopDAOImpl.java
@@ -10,110 +10,29 @@ import cz.fi.muni.pa165.entity.Troop;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  *
  * @author Andrej Nemec
  * @UCO 396474
  */
-public class TroopDAOImpl implements TroopDAO{
+ public class TroopDAOImpl implements TroopDAO{
 
-    private EntityManagerFactory emf;
-    
-    public TroopDAOImpl(EntityManagerFactory emf) {
-        this.emf = emf;
-    }
-        
-    @Override
-    public void createTroop(Troop troop) throws IllegalArgumentException {
-        
-        if(troop == null || troop.getId() != null || troop.getName() == null || 
-                troop.getMission()== null || troop.getAmountOfMoney() == null) {
-            throw new IllegalArgumentException("Create troop called with wrong param");
-        } 
-        EntityManager em = emf.createEntityManager();
-        em.getTransaction().begin();
-        em.persist(troop);
-        em.getTransaction().commit();
-        em.close();
-    }
+@Autowired EntityManager entityManager; public TroopDAOImpl(EntityManagerFactory emf) { } public EntityManager getEntityManager() { return entityManager; }
 
-    @Override
-    public Troop getTroop(Long id) throws IllegalArgumentException {
-        if(id == null)
-        {
-            throw new IllegalArgumentException("getTroop called with null");
-        }
-        EntityManager em = emf.createEntityManager();
-        em.getTransaction().begin();
-        Troop troop = em.find(Troop.class, id);
-        em.getTransaction().commit();
-        em.close();
-        return troop;
-    }
+public void setEntityManager(EntityManager entityManager) { this.entityManager = entityManager; } @Override public void createTroop(Troop troop) throws IllegalArgumentException { if(troop == null || troop.getId() != null || troop.getName() == null || troop.getMission()== null || troop.getAmountOfMoney() == null) { throw new IllegalArgumentException("Create troop called with wrong param"); } entityManager.persist(troop); }
 
-    @Override
-    public void updateTroop(Troop troop) throws IllegalArgumentException {
-        /**
-        if(troop == null){System.out.println("1");}
-        if(troop.getId() != null){System.out.println("2");}
-        if(troop.getName() == null){System.out.println("3");}
-        if(troop.getMission() == null){System.out.println("4");}
-        if(troop.getAmountOfMoney() == null){System.out.println("5");}
-        * */
-        if(troop == null || troop.getName() == null || 
-                troop.getMission()== null || troop.getAmountOfMoney()== null) {
-            throw new IllegalArgumentException("Update troop called with wrong param");
-        }   
-        EntityManager em = emf.createEntityManager();
-        em.getTransaction().begin();
-        em.merge(troop);
-        em.getTransaction().commit();
-        em.close();
-    }
+@Override public Troop getTroop(Long id) throws IllegalArgumentException { if(id == null) { throw new IllegalArgumentException("getTroop called with null"); } Troop troop = entityManager.find(Troop.class, id);
 
-    @Override
-    public void removeTroop(Troop troop) throws IllegalArgumentException {
-        if(troop == null) {
-            throw new IllegalArgumentException("Troop can't be null.");
-        }
-        if(troop.getId() == null) {
-            throw new IllegalArgumentException("Troop is not present in DB.");
-        } 
-        EntityManager em = emf.createEntityManager();
-        em.getTransaction().begin();
-        Troop present = em.find(Troop.class, troop.getId());
-        em.getTransaction().commit();
-        
-        if(present == null) {
-            throw new IllegalArgumentException("Troop is not present in DB.");
-        } else {
-            em.getTransaction().begin();   
-            em.remove(em.contains(troop) ? troop : em.merge(troop));
-            em.getTransaction().commit();
-        }
-        em.close();
-    }
+return troop; }
 
-    @Override
-    public List<Troop> getAllTroops() {
-        EntityManager em = emf.createEntityManager();
-        em.getTransaction().begin();
-        List<Troop> troops = em.createQuery("SELECT t FROM Troop t", Troop.class).getResultList();
-        em.getTransaction().commit();
-        em.close();
-        return  troops;
-    }
+@Override public void updateTroop(Troop troop) throws IllegalArgumentException { if(troop == null || troop.getName() == null || troop.getMission()== null || troop.getAmountOfMoney()== null) { throw new IllegalArgumentException("Update troop called with wrong param"); } entityManager.merge(troop); }
 
-    @Override
-    public List<Troop> findTroopByName(String name) throws IllegalArgumentException {
-        List<Troop> troop;
-        EntityManager em = emf.createEntityManager();
-        em.getTransaction().begin();
-        troop = em.createQuery("SELECT t FROM Troop t WHERE t.name = :name").setParameter("name", name).getResultList();
-        //query.setParameter("name", name);
-        em.getTransaction().commit();
-        em.close();
-        return troop;
-    }
-}
+@Override public void removeTroop(Troop troop) throws IllegalArgumentException { if(troop == null) { throw new IllegalArgumentException("Troop can't be null."); } if(troop.getId() == null) { throw new IllegalArgumentException("Troop is not present in DB."); } if (entityManager.contains(troop)){ entityManager.remove(troop); }else{ entityManager.remove(entityManager.merge(troop)); } }
+
+@Override public List<Troop> getAllTroops() { List<Troop> troops = entityManager.createQuery("SELECT t FROM Troop t", Troop.class).getResultList(); return troops; }
+
+@Override public List<Troop> findTroopByName(String name) throws IllegalArgumentException { List<Troop> troop; troop = entityManager.createQuery("SELECT t FROM Troop t WHERE t.name = :name").setParameter("name", name).getResultList();
+
+return troop; } }
