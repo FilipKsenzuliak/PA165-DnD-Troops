bfaf066737bbe9a45a12278adc27acc1a7cabc79
7Tomus <tomuso.o@gmail.com>
2014-11-07 19:47:41 +0100

    MissionDTO

--- DnDTroops-service/src/main/java/cz/fi/muni/pa165/dto/MissionDTO.java
+++ DnDTroops-service/src/main/java/cz/fi/muni/pa165/dto/MissionDTO.java
@@ -1,13 +1,60 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package cz.fi.muni.pa165.dto;
 
 /**
  *
- * @author Filip
+ * @author Tomas Javorsky
  */
 public class MissionDTO {
+    
+    private Long id;
+    
+    private String name;
+    
+    private String objective;
+    
+    private int reward;
+    
+    public MissionDTO(Long id){
+        this(id, null, null, 0); //int nemoze byt null, ak sa kvoli tomuto nieco dojebe tak sorry
+    }
+    
+    public MissionDTO(String name, String objective, int reward){
+        this(-1L, name, objective, reward);
+    }
+    
+    public MissionDTO(Long id, String name, String objective, int reward){
+        this.id = id;
+        this.name = name;
+        this.objective = objective;
+        this.reward = reward;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setObjective(String objective) {
+        this.objective = objective;
+    }
+
+    public void setReward(int reward) {
+        this.reward = reward;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getObjective() {
+        return objective;
+    }
+
+    public int getReward() {
+        return reward;
+    }
     
 }
